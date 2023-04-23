33f3fa3bf21b2da05dcbd70f85331180600aebe5
Nastya-L <asya.dermenzhi@gmail.com>
2023-04-23 18:49:12 +0300

    Added categories with loading from the server

--- src/js/index.js
+++ src/js/index.js
@@ -63,17 +63,104 @@ function createProduct(product) {
   ratingNumb.innerHTML = product.rating.rate;
   productBtn.innerHTML = 'Watch';
   productBtn.dataset.index = product.id;
+  div.dataset.category = product.category;
 
   return div;
 }
-
+// Обозначение загрузки
 const preloader = document.querySelector(".preloader");
 
-fetch('https://fakestoreapi.com/products')
+const urlProducts = decodeURIComponent(window.location.hash.substring(1));
+selectCategories(urlProducts);
+
+// Категории
+const categoriesСontent = document.querySelector(".categories__content");
+
+// Добавляем категории
+function categoriesAdd(el) {
+  const categoriesItem = document.createElement("li");
+  categoriesItem.classList = "categories__item";
+  
+  const categoriesLink = document.createElement("a");
+  categoriesLink.classList = "categories__link";
+  categoriesItem.appendChild(categoriesLink);
+
+  categoriesLink.innerHTML = el;
+  categoriesLink.dataset.category = el;
+
+  return categoriesItem;
+}
+
+// Запрос на сервер (категории)
+fetch('https://fakestoreapi.com/products/categories')
   .then(function (response) {
     return response.json()
   })
   .then(function (data) {
+    const urlProducts = decodeURIComponent(window.location.hash.substring(1));
+    for (let i = 0; i < data.length; i++) {
+      const categoriesLi = categoriesAdd(data[i]);
+      categoriesСontent.appendChild(categoriesLi);
+    }
+    preloader.classList.add('none');
+    const link = categoriesСontent.querySelector(`[data-category="${urlProducts}"]`);
+    if (link !== null) {
+      setActiveMenuLink(link);
+    }
+  })
+  .catch(function (error) {
+    console.log('Что-то пошло не так', error);
+  })
+
+  // Нажимаем на категорию
+categoriesСontent.addEventListener('click', (e) => {
+  if (e.target.hasAttribute('data-category') == false) {
+    return;
+  }
+  setActiveMenuLink(e.target);
+  selectCategories(e.target.dataset.category);
+  window.location.hash = e.target.dataset.category;
+  e.preventDefault();
+});
+
+// Активная категория
+function setActiveMenuLink(el) {
+  const curentLink = categoriesСontent.querySelector(".active");
+  if (curentLink !== null) {
+    curentLink.classList.remove("active");
+  }
+  el.classList.add("active");
+}
+
+// Путь на выбраную категорию
+function selectCategories(categ) {
+  let url = "https://fakestoreapi.com/products/"; 
+  if (categ == "electronics") {
+    url = 'https://fakestoreapi.com/products/category/electronics';
+  }
+  if (categ == "jewelery") {
+    url = 'https://fakestoreapi.com/products/category/jewelery';
+ }
+  if (categ == "men's clothing") {
+    url = "https://fakestoreapi.com/products/category/men's clothing";
+  }
+  if (categ == "women's clothing") {
+    url = "https://fakestoreapi.com/products/category/women's clothing";
+  }
+  requestProducts(url);
+}
+
+// Запрос на сервер по выбранной категории
+function requestProducts(url) {
+  preloader.classList.remove('none');
+  fetch(url)
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (data) {
+    while (content.hasChildNodes()) {  
+      content.removeChild(content.firstChild);
+    }
     for (let i = 0; i < data.length; i++) {
       const productDiv = createProduct(data[i]);
       content.appendChild(productDiv);
@@ -81,7 +168,6 @@ fetch('https://fakestoreapi.com/products')
     preloader.classList.add('none');
   })
   .catch(function (error) {
-    console.log('Что-то пошло не так', error);
+    console.log('Что-то пошло не так22', error);
   })
-
-
+}
